6b10e595223dbb90a8148bee67c1e7a943a51b4c
Zigecek <honza007cz@hotmail.com>
2022-06-20 20:32:42 +0200

    Mongoose install

--- api.js
+++ api.js
@@ -4,7 +4,18 @@ require("./utils/mongoose").init();
 const express = require("express");
 const app = express();
 
-app.use(require("./routes/adam"));
+app.use(express.json());
+
+const routes = [
+  {
+    path: "/adam",
+    router: require("./routes/adam"),
+  },
+];
+
+routes.forEach((route) => {
+  app.use(route.path, route.router);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`API's working on port: ${process.env.PORT}`);
