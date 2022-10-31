33b95c2a910b737108629d2c8ac404d73f51737d
Jan Kozohorský <honza007cz@hotmail.com>
2022-10-31 12:06:10 +0100

    asfafaf

--- models/sos.js
+++ models/sos.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const odpovedSchema = mongoose.Schema({
+const sosSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   stanoviste: String,
   cas: Number,
 });
 
-module.exports = mongoose.model("Odpoved", odpovedSchema, "odpovedi");
+module.exports = mongoose.model("Sos", sosSchema, "soska");
